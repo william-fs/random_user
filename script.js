27aaf7e8820677322944f52a96f06c844cce2fc0
William Ferreira <wfs19@live.com>
2023-10-10 13:58:25 -0300

    Finalizado a lógica de requisição da API e implementação no html

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Ligh/Dark Mode
-
 let modeColor = document.querySelector("#mode-color");
 let checkCenter = document.querySelector(".check-center");
 let lightIcon = document.querySelector(".light-icon");
 let darkIcon = document.querySelector(".dark-icon");
-
 
 modeColor.addEventListener("change", (event) => {
     if(event.currentTarget.checked) {
@@ -20,13 +18,36 @@ modeColor.addEventListener("change", (event) => {
     }
 })
 
-// Consumo API fake user 
-fetch("https://randomuser.me/api/?results=1")
+// Consumo API fake user
+const btnGerar = document.querySelector("#btnGerar");
+btnGerar.addEventListener("click", makeUser);
+
+function makeUser() {
+    fetch("https://randomuser.me/api/?results=1")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
+        let userName = document.querySelector("#userName");
+        let imgUser = document.querySelector("#imgUser");
+        let userAddress = document.querySelector("#userAddress");
+        let userFone = document.querySelector("#userFone");
+        let userMail = document.querySelector("#userMail");
         let user = data.results[0];
         let name = `${user.name.title} ${user.name.first} ${user.name.last}`;
-        console.log(name);
+        let userImgLarge = user.picture.large;
+        let adress = user.location;
+
+        imgUser.setAttribute("src", userImgLarge);
+        userName.innerText = name;
+        userAddress.innerText = `Rua ${adress.street.name}, ${adress.street.number} - Cidade: ${adress.city} - Estado: ${adress.state} - País: ${adress.country}`;
+        userFone.innerText = `Telefone: ${user.phone}`;
+        userMail.innerText = `E-mail: ${user.email}`;
     })
+    .catch((error) => {
+        console.log("Error");
+        console.log(error);
+        userName.innerText = "Error, try again please"
+    })
+}
+
